f6e9785638c698964209abe6e855ef29cd23cd27
William Moody <oswin795@outlook.com>
2019-10-30 14:06:33 -0600

    First Commit

---
+++ LearningUnitTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningUnitTesting
+//
+//  Created by William Moody on 10/30/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Worlds")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
